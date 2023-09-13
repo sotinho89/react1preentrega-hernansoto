3e8d2860672656527999bc2142987a20e9e06dce
hernansoto <sother32@gmail.com>
2023-09-13 18:47:05 -0300

    boostrap

--- primera/src/App.js
+++ primera/src/App.js
@@ -1,4 +1,21 @@
+import NavBar from './component/navBar/navBar';
+import './App.css'
 
+
+function App(){
+    return
+    (
+        <div>
+            <NavBar />
+        </div>
+    )
+}
 
 export default App;
-git
+
+
+
+
+
+
+
